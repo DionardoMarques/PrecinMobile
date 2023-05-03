cec578651c28a7f1517f365eff618fd55de9375d
DionardoMarques <dionardogianluca@gmail.com>
2023-05-03 17:21:44 -0300

    Personalized fonts

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+	useFonts,
+	Inter_400Regular,
+	Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_900Black });
+
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
